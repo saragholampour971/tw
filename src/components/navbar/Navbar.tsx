bb27d53fafaed618b274b2e6fac510569b722eb1
sara <saragholampour971@yahoo.com>
2025-04-23 16:10:16 +0200

    fix navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,26 @@
 import DesktopNavbar from "src/components/navbar/DesktopNavbar";
 import MobileNavbar from "src/components/navbar/MobileNavbar";
+import {useEffect, useRef, useState} from "react";
 
 
 const Navbar = () => {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(() => {
+      setIsDesktop(window.innerWidth >= 768);
+    });
+
+    resizeObserver.observe(document.body);
+
+    return () => resizeObserver.disconnect();
+  }, []);
 
   return (
-    <>
-      <div className={'hidden md:block sticky top-0 z-50'}>
-        <DesktopNavbar/>
-      </div>
-      <div className={'block md:hidden sticky sticky top-0 z-50'}>
-        <MobileNavbar/>
-      </div>
-    </>
-    
+    <div className="sticky top-0 z-50">
+      {isDesktop ? <DesktopNavbar/> : <MobileNavbar/>}
+    </div>
+
   );
 };
 
